97d95d9fa27ac759e26d35bc33eacef6c7bf31db
Jordan Porter <porter.jordan@gmail.com>
2015-11-02 11:37:17 -0500

    unload extern on nav switch

--- javascripts/navigation.js
+++ javascripts/navigation.js
@@ -1,20 +1,21 @@
 function setState(which){
-	$("div.content").hide();
-	var toShow = "div.content#" + which;
-	var toLoad = "div.content#" + which + " div.extern";
-	var loadingUrl = "./externs/"+ which + ".html";
+   $("div.content div.extern").empty();
+   $("div.content").hide();
+   var toShow = "div.content#" + which;
+   var toLoad = "div.content#" + which + " div.extern";
+   var loadingUrl = "./externs/"+ which + ".html";
 
-	$(toLoad).load(loadingUrl);
-	$(toShow).show();
+   $(toLoad).load(loadingUrl);
+   $(toShow).show();
 };
 
 $(document).ready(function(){
-	setState("Home");
-	
-	$("div#menu li").each(function(index) {
-		$(this).click(function() {
-			setState($(this).text());
-		});
-	});
+   setState("Home");
+
+   $("div#menu li").each(function(index) {
+      $(this).click(function() {
+      setState($(this).text());
+      });
+   });
 });
 
