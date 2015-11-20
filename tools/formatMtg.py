96dc3d814750f533c2bb63e3775fd6800441a1d1
Jordan Porter <porter.jordan@gmail.com>
2015-11-19 22:03:11 -0500

    Updated formatter code slightly.

--- tools/formatMtg.py
+++ tools/formatMtg.py
@@ -2,6 +2,65 @@
 
 from AdvancedHTMLParser import *
 
+class MtgConsts:
+    RED='R'
+    BLUE='U'
+    HYBRID='/'
+    BLACK='B'
+    GREEN='G'
+    WHITE='W'
+
+    FOREST='Forest'
+    PLAINS='Plains'
+    SWAMP='Swamp'
+    MOUNTAIN='Mountain'
+    ISLAND='Island'
+    
+def getColors(item):
+    returnList = None
+
+    for entry in item:
+        try:
+            if "img" == entry.tagName:
+                if returnList is None:
+                    returnList = []
+                    
+                altAttr = entry.getAttribute("alt")
+                if MtgConsts.GREEN in altAttr and MtgConsts.GREEN not in returnList:
+                    returnList.append(MtgConsts.GREEN)
+                if MtgConsts.BLUE in altAttr and MtgConsts.BLUE not in returnList:
+                    returnList.append(MtgConsts.BLUE)
+                if MtgConsts.BLACK in altAttr and MtgConsts.BLACK not in returnList:
+                    returnList.append(MtgConsts.BLACK)
+                if MtgConsts.WHITE in altAttr and MtgConsts.WHITE not in returnList:
+                    returnList.append(MtgConsts.WHITE)
+                if MtgConsts.RED in altAttr and MtgConsts.RED not in returnList:
+                    returnList.append(MtgConsts.RED)
+                if MtgConsts.HYBRID in altAttr and MtgConsts.HYBRID not in returnList:
+                    returnList.append(MtgConsts.HYBRID)
+        except AttributeError:
+            # Not an element, skip.
+            continue
+            
+    return returnList
+
+def isItemBasicLand(item):
+    for entry in item:
+        try:
+            if "a" == entry.tagName:
+                linkText = entry.innerHTML
+                
+                if (MtgConsts.ISLAND in linkText or
+                MtgConsts.SWAMP in linkText or
+                MtgConsts.MOUNTAIN in linkText or
+                MtgConsts.PLAINS in linkText or
+                MtgConsts.FOREST in linkText):
+                    return True
+        except AttributeError:
+            # Not an element, skip.
+            continue
+    return False
+    
 parser = AdvancedHTMLParser("test.html")
 
 maindeck = parser.getElementsByClassName("maindeck")[0]
@@ -55,6 +114,12 @@ print """<img src="" id="card_pic" style="max-height: 223px; max-width: 310px; t
 for (title, items) in titlesToEntries.iteritems():
     print title
     for item in items:
-        for entry in item:
-            print entry
-
+        colors = getColors(item)
+        if colors is not None:
+            print colors
+        else:
+            print "Colorless"
+            
+        if isItemBasicLand(item):
+            print "BASIC LAND"
+            
